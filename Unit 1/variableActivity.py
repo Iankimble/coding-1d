71e7d3804a3bf1faec355289eb2386ca50bc2795
Ian <38480036+Iankimble@users.noreply.github.com>
2026-09-09 15:24:57 +0000

    Class notes on variables and operators

--- Unit 1/variableActivity.py
+++ Unit 1/variableActivity.py
@@ -15,65 +15,38 @@ Ja3SneakersSKU = "91022382913"
 Douglas42="" 
 S21average="" 
 
-
 # When we want to create a variable, we make up a word, 
 # use the equal sign and then assign it a data type.  
 # whenever the computer sees our variable word it will
 # alway use the data inside. 
 
+# Variable Naming Rules
+# 1. A variable MUST start with letter and cannot start with a number
+# 2. A varaible can be a mixd of letters and numbers BUT not symbols 
+# 3. A variable CANNOT be a Python keyword
 
+# Variable Naming Conventions
+# we use naming conventions because we CANNOT have spaces in 
+# variable names
 
+# Camel Case - when creating variable names with multiple words,
+# every new word begins with a capital letter
+# the reason why its called camel case is bc every new word 
+# represents a hump.
+usernameForNewInstagramAccounts= 0
 
+# Snake Case - when creating a variable using snake case
+# we use a underscore symbol to space out each new word
+#the reason why its called snake case is bc the underscore
+# represents a snake on the ground
+username_for_new_instagram_accounts = 0
 
+# Pascal Case - pascal case is the same as camel case EXCEPT
+# all words start with a capital letter
+# its called pascal case bc its the same way we write first/ last names
+UsernameForNewInstagramAccounts= 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-name= "Ian Kimble" 
-yearsofExp= 10
-graduate= True
-
-Douglas42= '' 
-Savage21= ''
-thomas3 =''
-j2ordan3= ''
-
-age = 10
-Age = 20
-AGE = 30
-
-print(age)
-print(AGE)
-print(Age)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 

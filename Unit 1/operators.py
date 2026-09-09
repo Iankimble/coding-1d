d16e96e9aa90c07c9860af100cbdf3b4972b8ee5
Ian <38480036+Iankimble@users.noreply.github.com>
2026-09-09 15:25:19 +0000

    class notes on data variables and operators

---
+++ Unit 1/operators.py
@@ -0,0 +1,32 @@
+# Assignment Operators- Exclusively used to 
+# assign values to variables.
+# think key / value pairings
+
+# We use a single equal sign to represent the assingment
+# operator
+name = "Kimble"
+grade = 10
+school = True
+
+# Arithmetic Operators- Used on numerical 
+# data types to perform calculations. 
+# intergers (whole numbers) and floats (decimal numbers)
+
+# print is a function that lets us show code 
+# in the terminal
+print(3 + 3) # addition operator
+print(3.9 - 3) # subtraction operator
+print(12 / 3) # division operator
+print(3 * 4) # multiplication operator
+
+
+
+# Comparison Operators - Set of symbols used 
+# to assess if data is the same or different and
+# how they differ
+
+print(10 > 1) # greater than operator
+print(10 < 2) # less than operator
+
+# 2 equal signs compare if something is the same
+print("ian" == "Ian") # same as (false)

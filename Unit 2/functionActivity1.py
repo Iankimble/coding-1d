c33929aea91f98d7c326066ed24ff2884938ac96
Ian <38480036+Iankimble@users.noreply.github.com>
2026-09-25 11:36:08 +0000

    git ignore

--- Unit 2/functionActivity1.py
+++ Unit 2/functionActivity1.py
@@ -1,12 +1,21 @@
-# notes
-# we are comparing the number 85 to a number we type in
-# using input()
-# if the number is greater than 85 than we print true
-# it it is lower we print false
+# 1
+num1= input("please type in a number: ")
+print(int(num1) > 85)
 
-# num1 = input("please type in a number: ")
-# print(int(num1) > 85)
+#2
+val = input("hey you can write somewthing: ") 
+# input always take data  as string
+print(int(val) + 360)
 
-# 2.
-num2= input(" hey im here type something!!")
-print(360 + int(num2))
+val = input("hey you can write somewthing: ") 
+num= 360
+print(int(val) + num)
+
+#3A
+val= input()
+val2= "Boys Latin"
+print(val == val2)
+
+#3B
+val= input("")
+print(val == "Boys Latin")
